ce068d0aba84b315280f38eb5a5406774ba8b57d
George Lemon <georgelemon@protonmail.com>
2026-04-17 13:32:39 +0300

    add comments
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/supranim_session.nim
+++ src/supranim_session.nim
@@ -8,3 +8,17 @@
 
 import ./supranim_session/controller/[login, register, forgot]
 export login, register, forgot
+
+## This module provides a session management service for Supranim applications.
+## It defines a `SessionService` that can be used to create, manage, and clean up user sessions.
+##
+## The service provides controller procedures for handling user login, registration,
+## password reset flows and templates for Tim Engine.
+## 
+## Also, it generates and manages CSRF tokens for form submissions and provides a simple API
+## for storing flash notifications in the user session that can be displayed after redirects
+## or form submissions.
+## 
+## For using this Supranim package you can take a look at the [Supranim Starter Kit](https://github.com/supranim/app)
+## which is a boilerplate Supranim application that includes the necessary setup for using
+## the `SessionService` along with example routes and templates for user authentication flows
